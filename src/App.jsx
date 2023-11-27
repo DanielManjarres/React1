e8ae5f61f9fe76730bc515fe24557fdc30b4d9cc
Daniel <danielmanjarres6@gmail.com>
2023-11-26 23:34:32 -0500

    primero

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="perfil">
+      <header>
+        <h1>Mi Perfil Profesional</h1>
+      </header>
+      <div className="container">
+        <section className="section">
+          <h2>Información Personal</h2>
+          <p><strong>Nombre:</strong> Daniel Felipe Manjarres Clavijo</p>
+          <p><strong>Ocupacion:</strong>Estudiante</p>
+          <p><strong>Correo Electrónico:</strong> dmanjarres4711@cue.edu.co</p>
+          <p><strong>Teléfono:</strong> +57 31234573738</p>
+          <p><strong>Ubicación:</strong> Armenia/Quindio/Colombia</p>
+        </section>
+        <section className="section">
+          <h2>Conocimientos</h2>
+          <p> Tengo conocimientos básicos de manejo en las tecnologias, programación en HTML, Javascript, Typescript</p>
+        </section>
+        <section className="section">
+          <h2>Educación</h2>
+          <p><strong> Alexander Von Humboldt</strong> - Ingenieria De Software (Primer semestre)</p>
+        </section>
+        <section className="section">
+          <h2>Experiencia Laboral</h2>
+          <p><strong> Ninguna</strong></p>
+        </section>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
